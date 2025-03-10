3d506ade5069df2a426dc9a18a1fd5658810b2b3
Jorge <jmedina9@ur.rochester.edu>
2025-03-10 16:46:08 -0400

    migrating packmol tool to aviary

--- mdcrow/ldp_env/preprocess_tools/__init__.py
+++ mdcrow/ldp_env/preprocess_tools/__init__.py
@@ -1,5 +1,11 @@
 from .clean_tools import clean_pdb_file
+from .packing import pack_molecules
 from .pdb_get import download_pdb_file
 from .small_mol import get_small_molecule_PDB
 
-__all__ = ["download_pdb_file", "clean_pdb_file", "get_small_molecule_PDB"]
+__all__ = [
+    "download_pdb_file",
+    "clean_pdb_file",
+    "get_small_molecule_PDB",
+    "pack_molecules",
+]
